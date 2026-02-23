1f24765ff33f36e7f28365c2729274fe540ff224
rokkoryy <103527211+rokkoryy@users.noreply.github.com>
2026-02-23 23:29:05 +0300

    1.ts

--- 1.ts
+++ 1.ts
@@ -1,35 +1,31 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Visa = /** @class */ (function () {
-    function Visa() {
-        this._country = ["Арстоцка", "Колечия", "Орбистан"][~~(Math.random() * 3)];
+export default class Visa {
+    private _country: string;
+    private _isValid: boolean;
+    private _year: number;
+
+    constructor() {
+        const countries = ["Арстоцка", "Колечия", "Орбистан"];
+        this._country = countries[Math.floor(Math.random() * countries.length)];
         this._isValid = Math.random() < 0.5;
-        this._year = ~~(Math.random() * (2050 - 2000) + 2000);
+        this._year = Math.floor(Math.random() * (2050 - 2000) + 2000);
+
     }
-    Object.defineProperty(Visa.prototype, "country", {
-        get: function () {
-            return this._country;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Visa.prototype, "isValid", {
-        get: function () {
-            return this._isValid;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Visa.prototype, "year", {
-        get: function () {
-            return this._year;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Visa.prototype.showInfo = function () {
-        console.log("Страна:", this._country, "\nВалидность:", this._isValid, "\nГод выдачи", this._year);
-    };
-    return Visa;
-}());
-exports.default = Visa;
+
+    get country(): string {
+        return this._country;
+    }
+
+    get isValid(): boolean {
+        return this._isValid;
+    }
+
+    get year(): number {
+        return this._year;
+    }
+
+    showInfo(): void {
+        console.log(`Страна: ${this._country}`);
+        console.log(`Валидность: ${this._isValid}`);
+        console.log(`Год выдачи: ${this._year}`);
+    }
+}
